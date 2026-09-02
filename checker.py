0c393a912930663aff9c42c16ebdff08a65c1938
DavidOne10 <superpesdavide@gmail.com>
2026-09-02 17:51:43 +0200

    Update checker.py

--- checker.py
+++ checker.py
@@ -107,7 +107,7 @@ for asset_name, symbol in ASSETS.items():
     high_low = df['High'] - df['Low']
     high_close = np.abs(df['High'] - df['Close'].shift())
     low_close = np.abs(df['Low'] - df['Close'].shift())
-    df['ATR'] = np.max(pd.concat([high_low, high_close, low_close], axis=1), axis=1).rolling(14).mean()
+    df['ATR'] = np.max(pd.concat([high_low, high_close, low_close], axis=1), axis=1).rolling(14, min_periods=1).mean()
 
     today_date = df.index.date.max()
     today_bars = df[df.index.date == today_date]
